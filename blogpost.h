42543a253ac544d9583e741f4300b4259e1105c0
Daryl Stark <daryl.stark@hightechcampus.com>
2023-11-09 13:41:19 +0000

    First draft

---
+++ blogpost.h
@@ -0,0 +1,43 @@
+#ifndef __BLOGPOST_H__
+#define __BLOGPOST_H__
+
+#include <string>
+#include <memory>
+#include <vector>
+
+#include "blogpoststate.h"
+
+class BlogPost
+{
+    friend class BlogPostState;
+    friend class BlogPostDraftState;
+    friend class BlogPostPublishedState;
+
+protected:
+    std::shared_ptr<BlogPostState> _state;
+    std::vector<std::string> _history;
+    std::vector<std::string> _comments;
+    std::string _text;
+    bool _published;
+    bool _archived;
+
+private:
+    void __transition(std::shared_ptr<BlogPostState> state);
+
+public:
+    BlogPost();
+
+    // Behaviour
+    void edit(std::string new_text);
+    void publish();
+    void archive();
+    void unarchive();
+    void add_comment(std::string comment);
+
+    // Getters
+    const std::string &get_text() const;
+    const std::vector<std::string> &get_history() const;
+    const bool is_published() const;
+};
+
+#endif
---
+++ blogpoststate.h
@@ -0,0 +1,61 @@
+#ifndef __BLOGPOSTSTATE_H__
+#define __BLOGPOSTSTATE_H__
+
+#include <string>
+
+class BlogPost;
+
+class BlogPostState
+{
+protected:
+    BlogPost &_context;
+
+public:
+    // BlogPostState() {}
+    BlogPostState(BlogPost &context);
+
+    // Abstract methods
+    virtual void edit(std::string new_text) = 0;
+    virtual void publish() = 0;
+    virtual void archive() = 0;
+    virtual void unarchive() = 0;
+    virtual void add_comment(std::string comment) = 0;
+};
+
+class BlogPostDraftState : public BlogPostState
+{
+public:
+    BlogPostDraftState(BlogPost &context);
+
+    void edit(std::string new_text);
+    void publish();
+    void archive();
+    void unarchive();
+    void add_comment(std::string comment);
+};
+
+class BlogPostPublishedState : public BlogPostState
+{
+public:
+    BlogPostPublishedState(BlogPost &context);
+
+    void edit(std::string new_text);
+    void publish();
+    void archive();
+    void unarchive();
+    void add_comment(std::string comment);
+};
+
+class BlogPostArchivedState : public BlogPostState
+{
+public:
+    BlogPostArchivedState(BlogPost &context);
+
+    void edit(std::string new_text);
+    void publish();
+    void archive();
+    void unarchive();
+    void add_comment(std::string comment);
+};
+
+#endif
